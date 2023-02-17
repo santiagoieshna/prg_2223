abbfc1ab3b9dc4133b9ce1b146b62e4ff4f2ae13
Portalo <smiguezc01@iesarroyoharnina.es>
2023-02-17 13:11:49 +0100

    T5_Ej7

---
+++ ProjectsJava/T5_Ejer7/src/Productos/ColeccionProductos.java
@@ -0,0 +1,38 @@
+package Productos;
+import Productos.*;
+public class ColeccionProductos {
+
+	private Productos[] productos;
+	
+	/**
+	 * 
+	 * @param id
+	 * @param temperatura
+	 */
+	public boolean modificarTemperaturaCongelacion(int id, double temperatura) {
+		int indice = buscarProducto(id);
+		boolean respuesta= false;
+		if(id!=-1){
+			Congelados item = (Congelados)productos[indice];
+			item.setTempRecomendada(temperatura);
+			respuesta =true;
+		}
+		return respuesta;
+	}
+	
+	/**
+	 * Devuelve el indice del array productos por su id
+	 * @param id
+	 * @return -1 si no existe en el array
+	 * 			el indice si existe
+	 */
+	private int buscarProducto(int id) {
+		for(int i=0 ; i<productos.length;i++) {
+			if(productos[i].getIdproducto()==id) {
+				return i;
+			}	
+		}
+		return -1;
+	}
+	
+}
---
+++ ProjectsJava/T5_Ejer7/src/Productos/Congelados.java
@@ -0,0 +1,37 @@
+package Productos;
+
+import java.time.LocalDate;
+
+public class Congelados extends Productos {
+	
+	private double tempRecomendada;
+	
+	/**
+	 * 
+	 * @param idproducto
+	 * @param descripcion
+	 * @param fechaCaducidad
+	 * @param numLote
+	 * @param tempRecomendada
+	 */
+	public Congelados(int idproducto, String descripcion, LocalDate fechaCaducidad, String numLote,
+			double tempRecomendada) {
+		super(idproducto, descripcion, fechaCaducidad, numLote);
+		this.tempRecomendada = tempRecomendada;
+	}
+
+	public double getTempRecomendada() {
+		return tempRecomendada;
+	}
+
+	public void setTempRecomendada(double tempRecomendada) {
+		this.tempRecomendada = tempRecomendada;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Congelados [tempRecomendada=" + tempRecomendada + "]";
+	}
+	
+}
