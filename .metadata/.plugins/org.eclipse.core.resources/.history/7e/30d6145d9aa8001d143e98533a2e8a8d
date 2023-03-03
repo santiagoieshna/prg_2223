9f0a9e1279c65b32ce0586a76e56ee61b78afb46
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-03-03 01:41:26 +0100

    Ej_7 T5

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7e/30d6145d9aa8001d143e98533a2e8a8d
@@ -0,0 +1,16 @@
+package programa;
+import programa.GestionPrograma;
+
+public class mainPrograma {
+
+	public static void main(String[] args) {
+		
+		
+		Programa prg = new Programa("Felipe","Developer", "Acantilado","11111","Hola","Hey",25);
+		
+		GestionPrograma.addPrograma(prg);
+		GestionPrograma.filterNombre("Felipe");
+
+	}
+
+}
--- ProjectsJava/T5_Ejer7/src/Productos/ColeccionProductos.java
+++ ProjectsJava/T5_Ejer7/src/Productos/ColeccionProductos.java
@@ -1,4 +1,6 @@
 package Productos;
+import java.util.Iterator;
+
 import Productos.*;
 public class ColeccionProductos {
 
@@ -26,13 +28,87 @@ public class ColeccionProductos {
 	 * @return -1 si no existe en el array
 	 * 			el indice si existe
 	 */
-	private int buscarProducto(int id) {
+	public int buscarProducto(int id) {
 		for(int i=0 ; i<productos.length;i++) {
 			if(productos[i].getIdproducto()==id) {
 				return i;
 			}	
 		}
 		return -1;
+	}
+	
+	/**
+	 * 
+	 * @param pais
+	 * @return
+	 */
+	public Productos[] consultarProductosCaducados(String pais){
+		Productos[] listaProductos=null;
+		int size = getSize(pais);
+		crearArray(size, listaProductos, pais);
+		return listaProductos;
+	}
+	
+	public Productos[] consultarProductosCaducados() {
+		
+		
+	}
+	
+	/**
+	 * Crea un array de Productos
+	 * @param size
+	 * @param listaProductos
+	 * @param pais
+	 */
+	private void crearArray(int size, Productos[] listaProductos , String pais) {
+		Productos[] frescosLista = new Productos[size];
+		int cont = 0;
+		for (int i = 0; i < productos.length; i++) {
+			if(productos[i] instanceof Frescos) {
+				Frescos fresh = (Frescos) productos[i];
+				if(fresh.getPaisOrigen().equals(pais)) {
+					frescosLista[cont] = productos[i];
+					cont ++;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Devuelve el numero de coincidencias con respecto a al parametro pais
+	 * @param pais
+	 * @return
+	 */
+	private int getSize(String pais) {
+		int cont=0;
+		
+		for (Productos producto : productos) {
+			if(producto instanceof Frescos) {
+				producto = (Frescos) producto;
+				if(((Frescos) producto).getPaisOrigen().equals(pais)) {
+					cont ++;
+				}
+			}
+		}
+		return cont;
+	}
+
+	/**
+	 * Devuelve el numero de coincidencias con respecto a al parametro pais
+	 * @param pais
+	 * @return
+	 */
+	private int getSizeTraditional(String pais) {
+		int cont=0;
+		for (int i = 0; i < productos.length; i++) {
+			if(productos[i] instanceof Frescos) {
+				Frescos pFresh = (Frescos) productos[i];
+				if(pFresh.getPaisOrigen().equals(pais)) {
+					cont ++;
+				}
+			}
+		}
+		return cont;
 	}
 	
 }
