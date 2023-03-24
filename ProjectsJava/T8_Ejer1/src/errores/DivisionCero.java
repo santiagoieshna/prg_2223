14db60154f661846f62e26192f6fd23c89b47362
Portalo <smiguezc01@iesarroyoharnina.es>
2023-03-24 13:14:40 +0100

    try Caths

---
+++ ProjectsJava/T8_Ejer1/src/errores/DivisionCero.java
@@ -0,0 +1,41 @@
+package errores;
+
+import java.util.Scanner;
+
+public class DivisionCero {
+
+	public static void main(String[] args) {
+		
+		int numerador;
+		int denominador;
+		float  division=0;
+		
+		try {
+			Scanner sc  = new  Scanner(System.in);
+			System.out.println("numerador");
+			numerador = sc.nextInt();
+			System.out.println("denominador");
+			denominador= sc.nextInt();
+			
+			division =  numerador / denominador;
+			
+		}catch ( ArithmeticException e) {
+			System.out.println("has divido entre zero");
+			System.out.println("ha ocurrido un "+e);
+			
+		}catch ( InputMismatchException e) {
+		System.out.println("has divido entre zero");
+		System.out.println("ha ocurrido un "+e);
+		
+		}
+		catch (Exception e) {
+		System.out.println("ha ocurrido un "+e.getMessage());
+		System.out.println("ha ocurrido un "+e);
+		
+		}
+		finally {
+			System.out.println(division);
+		}
+	}
+
+}
