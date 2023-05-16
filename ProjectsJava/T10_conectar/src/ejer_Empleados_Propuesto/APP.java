4d158bc9eedbca099b9b08966557b38665376610
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-05-16 02:23:47 +0200

    Ej Empleado APP

---
+++ ProjectsJava/T10_conectar/src/ejer_Empleados_Propuesto/APP.java
@@ -0,0 +1,36 @@
+package ejer_Empleados_Propuesto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import conector.ConectarBD;
+
+public class APP {
+	public static void main(String[] args) {
+		Connection conexion;
+		Properties acceso=new Properties();
+		String url, user,password;
+		Empleado empleado;
+		int opcionUsuario=10;
+		//Credenciales
+		url="jdbc:mysql://localhost/classicmodels";
+		user="santiago";
+		password="123456";
+		//Ponemos acceso
+		acceso.put("user", user);
+		acceso.put("password",password);
+		
+		
+		try {
+			conexion=ConectarBD.getConexion(url, acceso);
+			//Menu
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
